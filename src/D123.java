a51bf9e47a23d3e4916bda641b96b2948576dd9c
user <user@MH04>
2019-02-21 15:02:43 +0800

    fcu

--- src/D123.java
+++ src/D123.java
@@ -4,7 +4,7 @@ public class D123 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-     System.out.println("Hello World!");
+     System.out.println("fcu!");
 	}
 
 }
